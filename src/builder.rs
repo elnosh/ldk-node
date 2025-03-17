b1b2caf1ecb81236c5cf6601a0e44da0967addda
elnosh <elnosh@pm.me>
2025-03-17 07:19:06 -0500

    Add `NetworkMismatch` error type
    Return specific error if the node is restarted with a different network
    from the one it was first created with. This will be caught when trying
    to load the `BDK` wallet.

--- src/builder.rs
+++ src/builder.rs
@@ -63,7 +63,7 @@ use bdk_wallet::Wallet as BdkWallet;
 use bip39::Mnemonic;
 
 use bitcoin::secp256k1::PublicKey;
-use bitcoin::{BlockHash, Network};
+use bitcoin::{constants, BlockHash, Network};
 
 use bitcoin::bip32::{ChildNumber, Xpriv};
 use std::collections::HashMap;
@@ -168,6 +168,8 @@ pub enum BuildError {
 	WalletSetupFailed,
 	/// We failed to setup the logger.
 	LoggerSetupFailed,
+	/// The given network does not match the node's previously configured network.
+	NetworkMismatch,
 }
 
 impl fmt::Display for BuildError {
@@ -189,6 +191,9 @@ impl fmt::Display for BuildError {
 			Self::WalletSetupFailed => write!(f, "Failed to setup onchain wallet."),
 			Self::LoggerSetupFailed => write!(f, "Failed to setup the logger."),
 			Self::InvalidNodeAlias => write!(f, "Given node alias is invalid."),
+			Self::NetworkMismatch => {
+				write!(f, "Given network does not match the node's previously configured network.")
+			},
 		}
 	}
 }
@@ -903,10 +908,19 @@ fn build_with_store_internal(
 		.descriptor(KeychainKind::Internal, Some(change_descriptor.clone()))
 		.extract_keys()
 		.check_network(config.network)
+		.check_genesis_hash(constants::genesis_block(config.network).block_hash())
 		.load_wallet(&mut wallet_persister)
-		.map_err(|e| {
-			log_error!(logger, "Failed to set up wallet: {}", e);
-			BuildError::WalletSetupFailed
+		.map_err(|e| match e {
+			bdk_wallet::LoadWithPersistError::InvalidChangeSet(
+				bdk_wallet::LoadError::Mismatch(bdk_wallet::LoadMismatch::Network { .. }),
+			) => BuildError::NetworkMismatch,
+			bdk_wallet::LoadWithPersistError::InvalidChangeSet(
+				bdk_wallet::LoadError::Mismatch(bdk_wallet::LoadMismatch::Genesis { .. }),
+			) => BuildError::NetworkMismatch,
+			_ => {
+				log_error!(logger, "Failed to set up wallet: {}", e);
+				BuildError::WalletSetupFailed
+			},
 		})?;
 	let bdk_wallet = match wallet_opt {
 		Some(wallet) => wallet,
